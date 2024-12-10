f58db9469a62c692608aa8dd5dd63a1d89f564ce
Rahul Menon <menonrahul@gmail.com>
2024-12-10 12:57:34 -0600

    find the leader from the frontend on request

--- src/frontend.rs
+++ src/frontend.rs
@@ -15,7 +15,7 @@ pub mod rafters {
 
 use rafters::front_end_server::{FrontEnd, FrontEndServer};
 use rafters::key_value_store_client::KeyValueStoreClient;
-use rafters::{GetKey, IntegerArg, KeyValue, Reply};
+use rafters::{Empty, GetKey, IntegerArg, KeyValue, Reply, State};
 
 type ChildMap = HashMap<i32, (std::process::Child, KeyValueStoreClient<Channel>)>;
 
@@ -31,6 +31,38 @@ impl FrontendState {
         Self {
             servers: ChildMap::new(),
             current_leader: 0,
+        }
+    }
+
+    async fn find_leader(&mut self) -> i32 {
+        let mut terms_to_leaders: HashMap<i32, Vec<i32>> = HashMap::new();
+        for (id, (_, ref mut client)) in &mut self.servers {
+            let node_status = client.get_state(Empty {}).await;
+            if node_status.is_err() {
+                warn!(
+                    "Unable to query node {} for state. Err: {}",
+                    id,
+                    node_status.unwrap_err()
+                );
+                continue;
+            }
+            let resp = node_status.unwrap().into_inner();
+            if resp.is_leader {
+                terms_to_leaders.entry(resp.term).or_default().push(*id);
+            }
+        }
+
+        let last_term = terms_to_leaders.keys().copied().max().unwrap_or_default();
+        if last_term == 0 {
+            warn!("Trying to get leader but wasn't able to find any!");
+            0
+        } else if terms_to_leaders[&last_term].len() != 1 {
+            error!("Multiple leaders in last term!");
+            0
+        } else {
+            let leader = terms_to_leaders[&last_term][0];
+            info!("Found leader as {}", leader);
+            leader
         }
     }
 }
@@ -50,61 +82,27 @@ impl FrontEnd for RaftersFrontend {
     async fn get(&self, request: Request<GetKey>) -> Result<Response<Reply>, Status> {
         // ask leader for info
         let mut state = self.state.lock().await;
+        state.current_leader = state.find_leader().await;
         let leader_id = state.current_leader;
         if leader_id == 0 {
-            return Err(Status::unavailable("Raft cluster not started yet!"));
+            return Err(Status::unavailable("Unable to find leader!"));
         }
         let (_, leader_client) = &state.servers[&leader_id];
         let mut leader_client = leader_client.clone();
-        let getkey = request.into_inner();
-        match leader_client.get(Request::new(getkey.clone())).await {
-            Ok(response) => {
-                let response = response.into_inner();
-                if response.wrong_leader {
-                    // only retrying once to avoid looping a lot and save on complexity
-                    let new_leader: i32 = response.value.parse().unwrap();
-                    state.current_leader = new_leader;
-                    state.servers[&new_leader]
-                        .1
-                        .clone()
-                        .get(Request::new(getkey))
-                        .await
-                } else {
-                    Ok(Response::new(response))
-                }
-            }
-            Err(err) => Err(Status::internal(err.to_string())),
-        }
+        leader_client.get(request).await
     }
 
     async fn put(&self, request: Request<KeyValue>) -> Result<Response<Reply>, Status> {
         // tell leader
         let mut state = self.state.lock().await;
+        state.current_leader = state.find_leader().await;
         let leader_id = state.current_leader;
         if leader_id == 0 {
-            return Err(Status::unavailable("Raft cluster not started yet!"));
+            return Err(Status::unavailable("Unable to find leader!"));
         }
         let (_, leader_client) = &state.servers[&leader_id];
         let mut leader_client = leader_client.clone();
-        let getkey = request.into_inner();
-        match leader_client.put(Request::new(getkey.clone())).await {
-            Ok(response) => {
-                let response = response.into_inner();
-                if response.wrong_leader {
-                    // only retrying once to avoid looping a lot and save on complexity
-                    let new_leader: i32 = response.value.parse().unwrap();
-                    state.current_leader = new_leader;
-                    state.servers[&new_leader]
-                        .1
-                        .clone()
-                        .put(Request::new(getkey))
-                        .await
-                } else {
-                    Ok(Response::new(response))
-                }
-            }
-            Err(err) => Err(Status::internal(err.to_string())),
-        }
+        leader_client.put(request).await
     }
 
     async fn replace(&self, request: Request<KeyValue>) -> Result<Response<Reply>, Status> {
@@ -117,7 +115,6 @@ impl FrontEnd for RaftersFrontend {
         } else {
             "target/release/raftserver"
         };
-        let mut state = self.state.lock().await;
         let num_servers = request.into_inner().arg;
         let children: Vec<std::process::Child> = (1..=num_servers)
             .map(|i| {
@@ -132,21 +129,29 @@ impl FrontEnd for RaftersFrontend {
             })
             .collect();
 
-        // wait for raft nodees to come online before sending them messages
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-        for (i, child) in (1..=num_servers).zip(children) {
-            let endpoint = Endpoint::from_shared(format!("http://127.0.0.1:{}", 9000 + i)).unwrap();
-            info!("Connecting to node {} on {:?}", i, endpoint.uri());
-            let client = KeyValueStoreClient::connect(endpoint)
-                .await
-                .unwrap_or_else(|_| {
-                    error!("Couldn't connect to raft node {}", i);
-                    panic!()
-                });
-            state.servers.insert(i, (child, client));
-        }
+        let node_state = self.state.clone();
+        tokio::spawn(async move {
+            let mut state = node_state.lock().await;
+            // wait for raft nodees to come online before sending them messages
+            tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+            for (i, child) in (1..=num_servers).zip(children) {
+                let endpoint =
+                    Endpoint::from_shared(format!("http://127.0.0.1:{}", 9000 + i)).unwrap();
+                info!("Connecting to node {} on {:?}", i, endpoint.uri());
+                let client = KeyValueStoreClient::connect(endpoint)
+                    .await
+                    .unwrap_or_else(|_| {
+                        error!("Couldn't connect to raft node {}", i);
+                        panic!()
+                    });
+                state.servers.insert(i, (child, client));
+            }
 
-        state.current_leader = 1; // Guess that node 1 is the leader
+            // wait some time for leader election to happen
+            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+            state.current_leader = state.find_leader().await;
+        });
+
         Ok(Response::new(Reply {
             wrong_leader: false,
             error: String::from(""),
