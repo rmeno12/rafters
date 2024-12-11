610cd1b5f753c837a4ff82e9524148e2cf942950
Rahul Menon <menonrahul@gmail.com>
2024-12-11 11:18:34 -0600

    organize things into testcluster

--- tests/basic_startup.rs
+++ tests/basic_startup.rs
@@ -1,24 +1,19 @@
 mod common;
 
 use anyhow::Result;
-use common::{find_leader, rafters::IntegerArg, start_frontend, ClusterGuard};
+use common::TestCluster;
 use tokio::time::Duration;
-use tonic::Request;
 
 #[tokio::test]
 async fn test_cluster_startup() -> Result<()> {
-    let (child, mut client) = start_frontend().await;
-    let _guard = ClusterGuard { frontend: child };
+    let mut cluster = TestCluster::new();
+    let mut client = cluster.start_frontend().await?;
 
-    let n = 5;
-
-    let mut startup_request = Request::new(IntegerArg { arg: n });
-    startup_request.set_timeout(Duration::from_millis(1000));
-    client.start_raft(startup_request).await?;
+    cluster.start_cluster(5, &mut client).await?;
 
     tokio::time::sleep(Duration::from_secs(3)).await;
 
-    let leader = find_leader(n).await?;
+    let leader = cluster.find_leader().await?;
 
     println!("Got raft leader as {}", leader);
 
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,17 +2,19 @@ pub mod rafters {
     tonic::include_proto!("raftkv");
 }
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::{
     collections::HashMap,
     process::{Child, Command},
 };
 use thiserror::Error;
+use tokio::time::Duration;
 use tonic::transport::Channel;
+use tonic::Request;
 
 use rafters::front_end_client::FrontEndClient;
 use rafters::key_value_store_client::KeyValueStoreClient;
-use rafters::Empty;
+use rafters::{Empty, IntegerArg};
 
 #[derive(Debug, Error)]
 enum RaftersTestingError {
@@ -22,66 +24,95 @@ enum RaftersTestingError {
     MultipleLeaders,
 }
 
-pub struct ClusterGuard {
-    pub frontend: Child,
+pub struct TestCluster {
+    num_servers: i32,
+    frontend: Option<Child>,
 }
 
-impl Drop for ClusterGuard {
+impl Drop for TestCluster {
     fn drop(&mut self) {
-        if let Err(e) = Command::new("pkill").arg("raftserver").status() {
-            eprintln!("Unable to kill raftservers: {}", e);
-        }
-        if let Err(e) = self.frontend.kill() {
-            eprintln!("Unable to kill frontend: {}", e);
+        if let Some(ref mut child) = &mut self.frontend {
+            if let Err(e) = Command::new("pkill").arg("raftserver").status() {
+                eprintln!("Unable to kill raftservers: {}", e);
+            }
+            if let Err(e) = child.kill() {
+                eprintln!("Unable to kill frontend: {}", e);
+            }
         }
     }
 }
 
-pub async fn start_frontend() -> (Child, FrontEndClient<Channel>) {
-    let child = Command::new("target/debug/frontend")
-        .spawn()
-        .expect("Failed to start frontend");
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+impl TestCluster {
+    pub fn new() -> Self {
+        Self {
+            num_servers: 0,
+            frontend: None,
+        }
+    }
 
-    let client = FrontEndClient::connect("http://127.0.0.1:8001")
-        .await
-        .expect("Failed to connect to frontend server");
+    pub async fn start_frontend(&mut self) -> Result<FrontEndClient<Channel>> {
+        let child = Command::new("target/debug/frontend")
+            .spawn()
+            .context("Failed to start frontend")?;
+        self.frontend = Some(child);
 
-    (child, client)
-}
+        // wait a tiny bit for the frontend to actually start
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
-pub async fn find_leader(num_servers: i32) -> Result<i32> {
-    let mut terms_to_leaders: HashMap<i32, Vec<i32>> = HashMap::new();
-    for id in 1..=num_servers {
-        let mut client =
-            match KeyValueStoreClient::connect(format!("http://127.0.0.1:{}", 9000 + id)).await {
-                Ok(c) => c,
+        let client = FrontEndClient::connect("http://127.0.0.1:8001")
+            .await
+            .context("Failed to connect to frontend server")?;
+
+        Ok(client)
+    }
+
+    pub async fn start_cluster(
+        &mut self,
+        num_servers: i32,
+        client: &mut FrontEndClient<Channel>,
+    ) -> Result<()> {
+        let mut startup_request = Request::new(IntegerArg { arg: num_servers });
+        startup_request.set_timeout(Duration::from_millis(1000));
+        client.start_raft(startup_request).await?;
+        self.num_servers = num_servers;
+
+        Ok(())
+    }
+
+    pub async fn find_leader(&self) -> Result<i32> {
+        let mut terms_to_leaders: HashMap<i32, Vec<i32>> = HashMap::new();
+        for id in 1..=self.num_servers {
+            let mut client =
+                match KeyValueStoreClient::connect(format!("http://127.0.0.1:{}", 9000 + id)).await
+                {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("Unable to connect to Raft server {}. Err: {}", id, e);
+                        continue;
+                    }
+                };
+            let node_status = match client.get_state(Empty {}).await {
+                Ok(resp) => resp.into_inner(),
                 Err(e) => {
-                    eprintln!("Unable to connect to Raft server {}. Err: {}", id, e);
+                    eprintln!("Unable to query node {} for state. Err: {}", id, e);
                     continue;
                 }
             };
-        let node_status = match client.get_state(Empty {}).await {
-            Ok(resp) => resp.into_inner(),
-            Err(e) => {
-                eprintln!("Unable to query node {} for state. Err: {}", id, e);
-                continue;
+            if node_status.is_leader {
+                terms_to_leaders
+                    .entry(node_status.term)
+                    .or_default()
+                    .push(id);
             }
-        };
-        if node_status.is_leader {
-            terms_to_leaders
-                .entry(node_status.term)
-                .or_default()
-                .push(id);
         }
-    }
 
-    let last_term = terms_to_leaders.keys().copied().max().unwrap_or_default();
-    if last_term == 0 {
-        Err(RaftersTestingError::NoLeader.into())
-    } else if terms_to_leaders[&last_term].len() != 1 {
-        Err(RaftersTestingError::MultipleLeaders.into())
-    } else {
-        Ok(terms_to_leaders[&last_term][0])
+        let last_term = terms_to_leaders.keys().copied().max().unwrap_or_default();
+        if last_term == 0 {
+            Err(RaftersTestingError::NoLeader.into())
+        } else if terms_to_leaders[&last_term].len() != 1 {
+            Err(RaftersTestingError::MultipleLeaders.into())
+        } else {
+            Ok(terms_to_leaders[&last_term][0])
+        }
     }
 }
