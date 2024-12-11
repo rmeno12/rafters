f6af6b1f8f6b4c3eb8fedb3e09c2a28222b48f4a
Rahul Menon <menonrahul@gmail.com>
2024-12-11 17:26:24 -0600

    merge basic startup and nominal

--- tests/nominal.rs
+++ tests/nominal.rs
@@ -10,6 +10,23 @@ use tokio::time::Duration;
 
 #[tokio::test]
 #[serial]
+async fn test_cluster_startup() -> Result<()> {
+    let mut cluster = TestCluster::new();
+    let mut client = cluster.start_frontend().await?;
+
+    cluster.start_cluster(5, &mut client).await?;
+
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    let leader = cluster.find_leader().await?;
+
+    println!("Got raft leader as {}", leader);
+
+    Ok(())
+}
+
+#[tokio::test]
+#[serial]
 async fn test_single_put_get() -> Result<()> {
     let mut cluster = TestCluster::new();
     let mut client = cluster.start_frontend().await?;
