3b72d44744805af6a1aaa90218677b3cf01acb6e
Rahul Menon <menonrahul@gmail.com>
2024-12-11 10:47:11 -0600

    add basic cluster startup test

--- tests/basic_startup.rs
+++ tests/basic_startup.rs
@@ -1,20 +1,26 @@
 mod common;
 
-use common::{kill_cluster, rafters::IntegerArg, start_frontend};
-use tonic::{
-    Request, Response
-};
+use anyhow::Result;
+use common::{find_leader, rafters::IntegerArg, start_frontend, ClusterGuard};
 use tokio::time::Duration;
-
+use tonic::Request;
 
 #[tokio::test]
-async fn test_cluster_startup() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_cluster_startup() -> Result<()> {
     let (child, mut client) = start_frontend().await;
+    let _guard = ClusterGuard { frontend: child };
 
-    let mut startup_request = Request::new(IntegerArg{arg: 5});
+    let n = 5;
+
+    let mut startup_request = Request::new(IntegerArg { arg: n });
     startup_request.set_timeout(Duration::from_millis(1000));
     client.start_raft(startup_request).await?;
 
-    kill_cluster(child)?;
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    let leader = find_leader(n).await?;
+
+    println!("Got raft leader as {}", leader);
+
     Ok(())
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,11 +1,53 @@
-use std::process::{Child, Command};
-
 pub mod rafters {
     tonic::include_proto!("raftkv");
 }
 
-use rafters::front_end_client::FrontEndClient;
+use anyhow::Result;
+use std::{
+    collections::HashMap,
+    process::{Child, Command},
+};
 use tonic::transport::Channel;
+
+use rafters::front_end_client::FrontEndClient;
+use rafters::key_value_store_client::KeyValueStoreClient;
+use rafters::Empty;
+
+#[derive(Debug)]
+enum RaftersTestingError {
+    NoLeader,
+    MultipleLeaders,
+}
+
+impl std::error::Error for RaftersTestingError {}
+
+impl std::fmt::Display for RaftersTestingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            RaftersTestingError::NoLeader => {
+                write!(f, "No leader found in cluster")
+            }
+            RaftersTestingError::MultipleLeaders => {
+                write!(f, "Multiple leaders found in cluster for latest term")
+            }
+        }
+    }
+}
+
+pub struct ClusterGuard {
+    pub frontend: Child,
+}
+
+impl Drop for ClusterGuard {
+    fn drop(&mut self) {
+        if let Err(e) = Command::new("pkill").arg("raftserver").status() {
+            eprintln!("Unable to kill raftservers: {}", e);
+        }
+        if let Err(e) = self.frontend.kill() {
+            eprintln!("Unable to kill frontend: {}", e);
+        }
+    }
+}
 
 pub async fn start_frontend() -> (Child, FrontEndClient<Channel>) {
     let child = Command::new("target/debug/frontend")
@@ -20,10 +62,38 @@ pub async fn start_frontend() -> (Child, FrontEndClient<Channel>) {
     (child, client)
 }
 
-pub fn kill_cluster(mut frontend: Child) -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("pkill").arg("raftserver").status()?;
+pub async fn find_leader(num_servers: i32) -> Result<i32> {
+    let mut terms_to_leaders: HashMap<i32, Vec<i32>> = HashMap::new();
+    for id in 1..=num_servers {
+        let mut client =
+            match KeyValueStoreClient::connect(format!("http://127.0.0.1:{}", 9000 + id)).await {
+                Ok(c) => c,
+                Err(e) => {
+                    eprintln!("Unable to connect to Raft server {}. Err: {}", id, e);
+                    continue;
+                }
+            };
+        let node_status = match client.get_state(Empty {}).await {
+            Ok(resp) => resp.into_inner(),
+            Err(e) => {
+                eprintln!("Unable to query node {} for state. Err: {}", id, e);
+                continue;
+            }
+        };
+        if node_status.is_leader {
+            terms_to_leaders
+                .entry(node_status.term)
+                .or_default()
+                .push(id);
+        }
+    }
 
-    frontend.kill()?;
-
-    Ok(())
+    let last_term = terms_to_leaders.keys().copied().max().unwrap_or_default();
+    if last_term == 0 {
+        Err(RaftersTestingError::NoLeader.into())
+    } else if terms_to_leaders[&last_term].len() != 1 {
+        Err(RaftersTestingError::MultipleLeaders.into())
+    } else {
+        Ok(terms_to_leaders[&last_term][0])
+    }
 }
